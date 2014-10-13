c2b1d4986f41c69bbeeffa985ea2b80cb520d963
Yangjie Li(懒) <latyas@gmail.com>
2014-10-13 23:25:06 +0800

    Update pppoe_test.py

--- pppoe_test.py
+++ pppoe_test.py
@@ -19,7 +19,7 @@ class PPPOEHeartbeat:
 			ret ^= struct.unpack('!I', i)[0]
 		return ret
 
-	def _make_heartbeat(self, sip, challenge_seed):
+	def _make_heartbeat(self, sip, challenge_seed, first=False):
 		# DrcomDialExtProtoHeader - 5 bytes
 		"""
 		typedef struct _tagDrcomDialExtProtoHeader
@@ -31,48 +31,24 @@ class PPPOEHeartbeat:
 		} PPPOEHEADER;
 		"""
 		data = '\x07' # code
-		data += chr(self.count + 1) # id
+		data += chr(self.count) # id
 		data += '\x60\x00' # length
 		data += '\x03' # type
-		"""
-		struct _tagDrcomDialExtProtoLoginPacket {
-		    PPPOEHEADER header; // 5 byte
-
-		#define hcode   header.code     // PPPoE ext = 0x07
-		#define hid     header.id       // 自定义，每次不同就可以
-		#define hlength header.length   // 发送数据包的长度
-		#define htype   header.type     // PPPoE = 0x03
-
-		    unsigned char uidlength;
-		    unsigned char mac[6];
-		    unsigned long sip;
-		    unsigned long option;   //bit0=dhcp, bit1=请求封装, bit2-7(verno)
-		    //b8=(no first),b9=有tcpipdog.dll, b10-12 选择出口线路 2006-12-18
-		    //b13(mydll mark2007-2-28), bit14-31 unuse
-		    //b14(mydll mark b15=find proxy cut line (pppoe模式2007-11-22)
-		    //bit16-31 unuse
-
-		    unsigned char ChallengeSeed[SEED_LEN];
-		    unsigned long crc[2];
-
-		    //account ,len 0
-		    //struct _tagDrcomDialExtProtoNetWorkInfo netinfo[MAX_DRCOM_DIAL_EXT_PROTO_NET_NUM];
-		    //unsigned char unused[4];
-		};
-		"""
 		data += '\x00' # uid length
 		data += '\x00\x00\x00\x00\x00\x00' # mac
 		data += sip # AuthHostIP
-		data += '\x00\x62\x00\x14' # 非第一次则是 data += '\x00\x62\x00\x14' 
-		#data += struct.pack('I',0x14006200)
+		if first:
+			data += struct.pack("I", 0x6200) # 非第一次则是 data += '\x00\x62\x00\x14' 
+		else:
+			data += struct.pack("I", 0x6300)
 		data += challenge_seed # Challenge Seed
 		# CRC
-		# data += struct.pack('I',20000711) # DRCOM_DIAL_EXT_PROTO_CRC_INIT
+		data += struct.pack('I',20000711) # DRCOM_DIAL_EXT_PROTO_CRC_INIT
 		# data += '\xc7\x2f\x31\x01'
-		data + '\x01\x31\x2f\xc7'
-		#data += struct.pack('I',126)
+		# data + '\x01\x31\x2f\xc7'
+		data += struct.pack('I',126)
 		# data += '\x7e\x00\x00\x00'
-		data += '\x00\x00\x00\x7e'
+		#   data += '\x00\x00\x00\x7e'
 		# - DrcomDialExtProtoHeader end -
 		"""
 		struct _tagDrcomDialExtProtoNetWorkInfo
@@ -86,18 +62,17 @@ class PPPOEHeartbeat:
 		};
 		"""
 		data += '\x00'*16 # ip1
-    data += '\x00'*16 # ip2
-    data += '\x00'*16 # ip3
-    data += '\x00'*16 # ip4
+	    	data += '\x00'*16 # ip2
+	    	data += '\x00'*16 # ip3
+	    	data += '\x00'*16 # ip4
 
-		 
 		return data
 	def send(self):
-		pass
+		# 20s 发一次
+		# send challenge
+		# recv auth_required
+		# self.count = (self.count + 2) % 0xFF
+		# send auth packet
+		# self.count = (self.count + 2) % 0xFF
+		# recv success packet
 
-a = PPPOEHeartbeat(0x55)
-c = a._make_heartbeat('\xac\x15\x05\x0f','\xcf\x89\xa8\x03')
-print hexlify(c)
-print hex(a._DrcomCRC32(c))
-print hex((a._DrcomCRC32(c)*19680126))
-print hex((a._DrcomCRC32(c)*19680126) & 0xFFFFFFFF)
