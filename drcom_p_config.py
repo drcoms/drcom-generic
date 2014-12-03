66ed57afbbf2ee9952663494df9a4c5fdfebdecf
Yangjie Li(懒) <latyas@gmail.com>
2014-12-04 02:11:02 +0800

    Update drcom_p_config.py

--- drcom_p_config.py
+++ drcom_p_config.py
@@ -19,6 +19,7 @@ f = open(filename, 'rb')
 text = f.read()
 offset = re.search('\xf0\x00\xf0\x00....\x07.\x60\x00\x03\x00', text).start() + 8
 #print hexlify(text[offset:offset+330])
+print 'server = \'%s\'' % '.'.join([str(ord(i)) for i in text[offset-12:offset-8]])
 print 'pppoe_flag = \'%s\'' % hexed(text[offset+19])
 keep_alive2_flag = re.search('\xf0\x00\xf0\x00....\x07.\x5c\x28\x00\x0b\x03(.)\x02', text).group(1)
 #print hexlify(text[offset:offset+330])
