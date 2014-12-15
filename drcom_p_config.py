204cc7043a6be3a67246e6c1835b040e62c37d02
Yangjie Li(懒) <latyas@gmail.com>
2014-12-15 13:10:22 +0800

    Update drcom_p_config.py

--- drcom_p_config.py
+++ drcom_p_config.py
@@ -17,10 +17,10 @@ def hexed(s):
 filename = 'drp.pcapng'
 f = open(filename, 'rb')
 text = f.read()
-offset = re.search('\xf0\x00\xf0\x00....\x07.\x60\x00\x03\x00', text).start() + 8
+offset = re.search('\x07.\x60\x00\x03\x00', text).start()
 #print hexlify(text[offset:offset+330])
 print 'server = \'%s\'' % '.'.join([str(ord(i)) for i in text[offset-12:offset-8]])
 print 'pppoe_flag = \'%s\'' % hexed(text[offset+19])
-keep_alive2_flag = re.search('\xf0\x00\xf0\x00....\x07.\x5c\x28\x00\x0b\x03(.)\x02', text).group(1)
+keep_alive2_flag = re.search('\x07.\x5c\x28\x00\x0b\x03(.)\x02', text).group(1)
 #print hexlify(text[offset:offset+330])
 print 'keep_alive2_flag = \'%s\'' % hexed(keep_alive2_flag)
